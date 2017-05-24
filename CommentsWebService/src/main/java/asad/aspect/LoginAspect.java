9a109476f6be27b7f40a5798584474aedeb19934
Jérôme Moret <jermoret@gmail.com>
2017-05-24 12:58:50 +0200

    Add aspect around addComment()

--- CommentsWebService/src/main/java/asad/aspect/LoginAspect.java
+++ CommentsWebService/src/main/java/asad/aspect/LoginAspect.java
@@ -5,6 +5,7 @@
  */
 package asad.aspect;
 
+import asad.ws.User;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -14,19 +15,18 @@ import java.util.Arrays;
 @Aspect
 public class LoginAspect {
 
+    private IAuthentificationService authentificationService = new SimpleAuthentificationService();
+
     @Around("execution(* asad.ws.CommentsService.addComment(..))")
     public Object addCommentAround(ProceedingJoinPoint joinPoint) throws Throwable {
         Object[] args = joinPoint.getArgs(); // change the args if you want to
-        Object retVal = joinPoint.proceed(args); // run the actual method (or don't)
-        System.out.println("Return object : " + retVal);
-        return retVal; // return the return value (or something else)
-    }
-
-    @Around("execution(* asad.ws.CommentsService.deleteComment(..))")
-    public Object deleteCommentAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object[] args = joinPoint.getArgs(); // change the args if you want to
-        Object retVal = joinPoint.proceed(args); // run the actual method (or don't)
-        System.out.println("Return object : " + retVal);
-        return retVal; // return the return value (or something else)
+        User user = (User) args[1];
+        Object retVal;
+        if(authentificationService.IsValid(user.getLogin(), user.getPass())) {
+            retVal = joinPoint.proceed(args); // run the actual method (or don't)
+        } else {
+            retVal = false;
+        }
+        return retVal;
     }
 }
